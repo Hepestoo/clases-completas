852f1a765c17c207aa6648145e73b67a77682810
Hepestoo <teckalan2@gmail.com>
2024-12-16 12:26:50 -0500

    clase 17

--- practica4-backend/src/modules/auth/auth.service.ts
+++ practica4-backend/src/modules/auth/auth.service.ts
@@ -1,40 +1,47 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable } from "@nestjs/common";
 import { LoginAuthDto } from "./dto/login-auth.dto";
 import { JwtService } from "@nestjs/jwt";
 import * as bcrypt from 'bcrypt';
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "../users/entities/user.entity";
 import { Repository } from "typeorm";
+import { hash, compare } from "bcrypt";
+import { RegisterAuthDto } from "./dto/register-auth.dto";
 
 @Injectable()
 export class AuthService{
-    constructor(private jwtService: JwtService, @InjectRepository(User) private readonly userRepository: Repository<User>){}
-    login(credenciales: LoginAuthDto){
-        let payload={email:"teckalan2@gmail.com", id:1}
-        const token= this.jwtService.sign(payload)
-        return {token:token}; 
-    }
+    constructor(private jwtService:JwtService,
+      @InjectRepository(User) private userRepository: Repository<User>){}
 
-    async register(userData: { email: string; password: string; name:string }) {
-        const hashedPassword = await bcrypt.hash(userData.password, 10);
-        const newUser = this.userRepository.create({...userData, password: hashedPassword});
-        await this.userRepository.save(newUser);
-        return { message: 'Usuario registrado', user: { email: newUser.email } };
-      }
-    
-      async validateUser(email: string, password: string) {
-        const user = await this.userRepository.findOne({ where: { email } });
-        if (user && (await bcrypt.compare(password, user.password))) {
-          return user;
-        }
-        return null;
-      }
-    
-      generateJwt(user: User) {
-        const payload = { username: user.name, sub: user.id };
-        return {
-          access_token: this.jwtService.sign(payload),
-        };
-      }
+  async register(objUser: RegisterAuthDto){
+      const {password}= objUser
+      const plainToHash = await hash(password,12)
+    // console.log(plainToHash)
+
+    objUser={...objUser, password:plainToHash}
+    //console.log("Despues:", objUser)
+
+    return this.userRepository.save(objUser)
+  }
+
+  async login(credenciales:LoginAuthDto){
+
+      const {email, password}= credenciales
+      const user= await this.userRepository.findOne({
+          where:{
+              email:email
+          }
+      })
+
+      if(!user) return new HttpException('Usuario no encontrado', 404);
+
+      const verificarPass = await compare(password, user.password)
+
+      if(!verificarPass) throw new HttpException('Password invalido', 401)
+
+          let payload={email:user.email, id:user.id}
+          const token = this.jwtService.sign(payload)
+          return {user:user, token:token};
+  }
 
 }
